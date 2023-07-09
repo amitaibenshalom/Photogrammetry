24f144950dd0d601d05ec733b832a487f814072f
Amitai Ben Shalom <90959307+amitaibenshalom@users.noreply.github.com>
2023-07-09 19:47:44 +0300

    Update CONSTS.py

--- CONSTS.py
+++ CONSTS.py
@@ -7,12 +7,12 @@ Optimal_grid = [[1, 1], [2, 1], [3, 1], [2, 2], [3, 2], [3, 2], [3, 3], [3, 3], 
                 [6, 5], [6, 6], [6, 6], [6, 6]
     , [6, 6], [6, 6], [6, 6]]
 # Optimal_grid[23] = [6,4] # maybe better for 24 images
-
 MIN_IMAGE_TO_SHOW = 1
 MAX_IMAGE_TO_SHOW = 24
 CAM_RES = (640,480) #(160,120),(320,240),(640,480),(800,600),(1024,768),(1280,720),(1280,1024),(1920,1080)
 MAIN_WINDOW = 'Image capure for photogrametria Daphna'
-WINDOW_SIZE = (1400, 800)  # (1280,720) #(1400, 800) # (1360 , 768) X,Y !
+WINDOW_SIZE = (640,480)  # (1280,720) #(1400, 800) # (1360 , 768) X,Y !
+SCALE = 640.0/CAM_RES[0] #to make the same size independent of the cam resolution
 
 PATH_OF_USBS = '/media/mada/'
 NAME_OF_FOLDER_IN_USB = 'photogrammetria'
@@ -43,3 +43,4 @@ Demo_Counter = 0
 TAKE_IMAGE_ON_ALL_CAMERAS = False
 # flag to indicate if we need to show live video or not
 SHOW_LIVE_VIDEO = True
+SHOW_IMAGE_AFTER_CAPTURE = True
