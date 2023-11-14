d2d9609a4f7a9a9c1a56291881d1bb64cae1fa85
Amitai Ben Shalom <amitaiami22@gmail.com>
2023-11-14 12:37:36 +0200

    files

--- test.py
+++ test.py
@@ -2,38 +2,42 @@ import subprocess
 import os
 import json
 
-def run_meshroom(input_path, output_path, config_path=None):
+def run_meshroom(meshroom_path, input_path, output_path, config_path=None):
     # Ensure the output directory exists
     os.makedirs(output_path, exist_ok=True)
-
-    # Define the Meshroom command
+    
+    # # Define the Meshroom command
     meshroom_cmd = [
-        'meshroom_batch.exe',  # Replace with the actual Meshroom batch command
-        '--input', input_path,
-        '--output', output_path
+        meshroom_path, 
+        '-i', input_path,
+        '-o', output_path
     ]
 
     # Add params option if a config file is specified
     if config_path:
-        meshroom_cmd.extend(['--params', config_path])
+        meshroom_cmd.extend(['-p', config_path])
 
     try:
         # Run Meshroom command
-        subprocess.run(meshroom_cmd, check=True)
+        result = subprocess.run(meshroom_cmd, shell=True, capture_output=True, text=True)
         print("Meshroom processing completed successfully.")
+        print("Output:", result.stdout)
+
     except subprocess.CalledProcessError as e:
         print(f"Error running Meshroom: {e}")
 
 if __name__ == "__main__":
 
-    meshroom_dir = 'C:/Users/amita/Downloads/Meshroom-2021.1.0-win64/Meshroom-2021.1.0'
-
-    input_directory = 'D:/Amitai_D/museum/photogrammetria/tests/test31_8_2023/pictures_31_8_2023_0'
+    meshroom_dir = r' C:\Users\mada\Downloads\Meshroom-2023.2.0-win64\Meshroom-2023.2.0\meshroom_batch.exe'
+    # meshroom_dir = r'C:\Users\mada\Downloads\Meshroom-2023.2.0-win64\Meshroom-2023.2.0'
+    input_directory = r"C:\Users\mada\Documents\photogrammetry\photogrammetry_data\2023-11-13-19-11-02-4916\images"
 
     # Replace 'path/to/output' with the desired output path
-    output_directory = 'D:/Amitai_D/museum/photogrammetria/tests/test_14_11_2023'
-
+    output_directory = r"C:\Users\mada\Documents\photogrammetry\Meshroom-Test"
+    print(meshroom_dir)
+    print(input_directory)
+    print(output_directory)
+    print("\n\n")
     # Replace 'path/to/your/config.json' with the actual path to your JSON configuration file
-    config_file = 'C:/Users/amita/PycharmProjects/Meshroom-Test/meshroom_settings.JSON'
-
-    run_meshroom(input_directory, output_directory, config_file)
+    # config_file = 'C:/Users/mada/Documents/photogrammetry/musem_photogrametry_project/settings_override.json'
+    run_meshroom(meshroom_dir, input_directory, output_directory)
--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 from vedo import *
-
+return
 mesh = Mesh("D:/Amitai_D/museum/photogrammetria/tests/OBJ_FILES/SHALEV/texturedMesh.obj", )
 
 mesh.texture("D:/Amitai_D/museum/photogrammetria/tests/OBJ_FILES/SHALEV/texture_1001.png", scale=0.1)
