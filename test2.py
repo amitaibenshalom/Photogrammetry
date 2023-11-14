482cd7c4ded889f387850b67fe4c270224868ec2
Amitai Ben Shalom <amitaiami22@gmail.com>
2023-11-15 00:43:55 +0200

    tried therading

--- test2.py
+++ test2.py
@@ -1,5 +1,7 @@
 from vedo import *
 import os
+import time
+import threading
 
 # mesh = Mesh("D:/Amitai_D/museum/photogrammetria/tests/OBJ_FILES/SHALEV/texturedMesh.obj")
 # mesh.texture("D:/Amitai_D/museum/photogrammetria/tests/OBJ_FILES/SHALEV/texture_1001.png", scale=0.1)
@@ -10,5 +12,21 @@ obj_file_path = os.path.join(photogrammetry_data_path, 'texturedMesh.obj')
 texture_file_path = os.path.join(photogrammetry_data_path, 'texture_1001.png')
 mesh = Mesh(obj_file_path)
 mesh.texture(texture_file_path, scale=0.1)
-# mesh.show(size=(screen_width, screen_height))
-mesh.show(size=("f", "f"))
+# mesh.show(size=("f","f"))
+
+thread = threading.Thread(target=mesh.show(size=("f", "f")))
+# print(time.time())
+thread.start()
+# print(time.time())
+thread.join(timeout=5)
+# print(time.time())
+if thread.is_alive():
+    print("Timeout reached. Interrupting the thread.")
+    # mesh.clean()
+    # thread.join()  # Wait for the thread to finish after interruption
+    exit()
+else:
+    # print(time.time())
+    print("Thread completed before timeout.")
+# print(time.time())
+print("done")
