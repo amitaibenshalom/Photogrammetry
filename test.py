2522aedb46149c8d330bfe6937367692fed00352
Amitai Ben Shalom <amitaiami22@gmail.com>
2023-11-14 10:31:38 +0200

    Added JSON file

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 import subprocess
 import os
+import json
 
-def run_meshroom(input_path, output_path):
+def run_meshroom(input_path, output_path, config_path=None):
     # Ensure the output directory exists
     os.makedirs(output_path, exist_ok=True)
 
     # Define the Meshroom command
     meshroom_cmd = [
-        'meshroom_batch',  # Replace with the actual Meshroom batch command
+        'meshroom_batch.exe',  # Replace with the actual Meshroom batch command
         '--input', input_path,
         '--output', output_path
     ]
+
+    # Add params option if a config file is specified
+    if config_path:
+        meshroom_cmd.extend(['--params', config_path])
 
     try:
         # Run Meshroom command
@@ -20,10 +25,15 @@ def run_meshroom(input_path, output_path):
         print(f"Error running Meshroom: {e}")
 
 if __name__ == "__main__":
-    # Replace 'path/to/your/images' with the actual path to your images
-    input_directory = 'path/to/your/images'
+
+    meshroom_dir = 'C:/Users/amita/Downloads/Meshroom-2021.1.0-win64/Meshroom-2021.1.0'
+
+    input_directory = 'D:/Amitai_D/museum/photogrammetria/tests/test31_8_2023/pictures_31_8_2023_0'
 
     # Replace 'path/to/output' with the desired output path
-    output_directory = 'path/to/output'
+    output_directory = 'D:/Amitai_D/museum/photogrammetria/tests/test_14_11_2023'
 
-    run_meshroom(input_directory, output_directory)
+    # Replace 'path/to/your/config.json' with the actual path to your JSON configuration file
+    config_file = 'C:/Users/amita/PycharmProjects/Meshroom-Test/meshroom_settings.JSON'
+
+    run_meshroom(input_directory, output_directory, config_file)
