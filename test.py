766a2a37961de8513580effeca0aae533e74a2fd
Amitai Ben Shalom <amitaiami22@gmail.com>
2024-01-07 11:36:44 +0200

    meshroom batch from python attempt

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import subprocess
 import os
-import json
 
-def run_meshroom(meshroom_path, input_path, output_path, config_path=None):
-    # Ensure the output directory exists
+def run_meshroom(meshroom_path, input_path, output_path, config_path=None, forceCompute=True):
     os.makedirs(output_path, exist_ok=True)
     
-    # # Define the Meshroom command
     meshroom_cmd = [
         meshroom_path, 
         '-i', input_path,
@@ -15,29 +12,26 @@ def run_meshroom(meshroom_path, input_path, output_path, config_path=None):
 
     # Add params option if a config file is specified
     if config_path:
-        meshroom_cmd.extend(['-p', config_path])
+        meshroom_cmd.extend(['--overrides', config_path])
+
+    if forceCompute:
+        meshroom_cmd.append('--forceCompute')
 
     try:
         # Run Meshroom command
         result = subprocess.run(meshroom_cmd, shell=True, capture_output=True, text=True)
         print("Meshroom processing completed successfully.")
-        print("Output:", result.stdout)
+        # print("Output:", result.stdout)
 
     except subprocess.CalledProcessError as e:
         print(f"Error running Meshroom: {e}")
 
 if __name__ == "__main__":
 
-    meshroom_dir = r' C:\Users\mada\Downloads\Meshroom-2023.2.0-win64\Meshroom-2023.2.0\meshroom_batch.exe'
-    # meshroom_dir = r'C:\Users\mada\Downloads\Meshroom-2023.2.0-win64\Meshroom-2023.2.0'
-    input_directory = r"C:\Users\mada\Documents\photogrammetry\photogrammetry_data\2023-11-13-19-11-02-4916\images"
-
-    # Replace 'path/to/output' with the desired output path
+    meshroom_dir = r'C:\Users\mada\Downloads\Meshroom-2023.2.0-win64\Meshroom-2023.2.0\meshroom_batch.exe'
+    input_directory = r"C:\Users\mada\Documents\photogrammetry\photogrammetry_data\2024-01-04-11-56-57-3854\images"
     output_directory = r"C:\Users\mada\Documents\photogrammetry\Meshroom-Test"
-    print(meshroom_dir)
-    print(input_directory)
-    print(output_directory)
-    print("\n\n")
-    # Replace 'path/to/your/config.json' with the actual path to your JSON configuration file
-    # config_file = 'C:/Users/mada/Documents/photogrammetry/musem_photogrametry_project/settings_override.json'
-    run_meshroom(meshroom_dir, input_directory, output_directory)
+    config_file = r"C:\Users\mada\Documents\photogrammetry\Meshroom-Test\settings_override"
+    print("input: " + input_directory)
+    print("output: " + output_directory)
+    run_meshroom(meshroom_dir, input_directory, output_directory, config_file)
