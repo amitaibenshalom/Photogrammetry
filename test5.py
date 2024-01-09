39cd1b9aba760a03176021191cb4c93c389af04a
Amitai Ben Shalom <amitaiami22@gmail.com>
2024-01-09 18:44:30 +0200

    update

--- test5.py
+++ test5.py
@@ -1,21 +1,14 @@
-import sys, pygame
+import pygame
 from pygame.locals import *
-from pygame.constants import *
 from OpenGL.GL import *
+from OpenGL.GLUT import *
 from OpenGL.GLU import *
-import time
-
-# IMPORT OBJECT LOADER
-from objloader import *
-
 
 # Initialize Pygame
 pygame.init()
 
-# Set the window size
+# Set up the Pygame window
 width, height = 800, 600
-
-# Create a Pygame window
 pygame.display.set_mode((width, height), DOUBLEBUF | OPENGL)
 
 # Set up the OpenGL perspective projection matrix
@@ -33,8 +26,36 @@ glClearDepth(1.0)
 glEnable(GL_DEPTH_TEST)
 glDepthFunc(GL_LESS)
 
+# Load the object file and associated MTL file
+obj_vertices = []
+obj_texture_coords = []
+obj_faces = []
+obj_file_path = r"C:\Users\mada\Documents\photogrammetry\photogrammetry_data\2024-01-01-10-35-52-0016\output\texturedMesh.obj"
+
+with open(obj_file_path, 'r') as obj_file:
+    for line in obj_file:
+        if line.startswith('v '):
+            vertices = line.split()[1:]
+            obj_vertices.append([float(v) for v in vertices])
+        elif line.startswith('vt '):
+            texture_coords = line.split()[1:]
+            obj_texture_coords.append([float(t) for t in texture_coords])
+        elif line.startswith('f '):
+            face_data = line.split()[1:]
+            obj_faces.append([list(map(int, vertex.split('/'))) for vertex in face_data])
+
 # Rotation angle
 angle = 0.0
+
+# Function to compute face normals
+def compute_normals():
+    normals = []
+    for face in obj_faces:
+        v1 = [obj_vertices[face[0][0] - 1][i] - obj_vertices[face[1][0] - 1][i] for i in range(3)]
+        v2 = [obj_vertices[face[2][0] - 1][i] - obj_vertices[face[1][0] - 1][i] for i in range(3)]
+        normal = [v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0]]
+        normals.append(normal)
+    return normals
 
 # Function to handle rendering
 def render():
@@ -46,49 +67,18 @@ def render():
     # Move the model-view matrix to the center of the object
     glTranslatef(0.0, 0.0, -5.0)
 
-    # Rotate around the local y-axis
+    # Rotate around the local Y-axis
     glPushMatrix()
-    glRotatef(angle, 0.0, 1.0, 0)
+    glRotatef(angle, 0.0, 1.0, 0.0)
 
-    # Draw your object (e.g., a cube)
-    glBegin(GL_QUADS)
-    # draw a 3d cube
-    # top
-    glColor3f(0.0, 1.0, 0.0)
-    glVertex3f(1.0, 1.0, -1.0)
-    glVertex3f(-1.0, 1.0, -1.0)
-    glVertex3f(-1.0, 1.0, 1.0)
-    glVertex3f(1.0, 1.0, 1.0)
-    # bottom
-    glColor3f(1.0, 0.5, 0.0)
-    glVertex3f(1.0, -1.0, 1.0)
-    glVertex3f(-1.0, -1.0, 1.0)
-    glVertex3f(-1.0, -1.0, -1.0)
-    glVertex3f(1.0, -1.0, -1.0)
-    # front
-    glColor3f(1.0, 0.0, 0.0)
-    glVertex3f(1.0, 1.0, 1.0)
-    glVertex3f(-1.0, 1.0, 1.0)
-    glVertex3f(-1.0, -1.0, 1.0)
-    glVertex3f(1.0, -1.0, 1.0)
-    # back
-    glColor3f(1.0, 1.0, 0.0)
-    glVertex3f(1.0, -1.0, -1.0)
-    glVertex3f(-1.0, -1.0, -1.0)
-    glVertex3f(-1.0, 1.0, -1.0)
-    glVertex3f(1.0, 1.0, -1.0)
-    # left
-    glColor3f(0.0, 0.0, 1.0)
-    glVertex3f(-1.0, 1.0, 1.0)
-    glVertex3f(-1.0, 1.0, -1.0)
-    glVertex3f(-1.0, -1.0, -1.0)
-    glVertex3f(-1.0, -1.0, 1.0)
-    # right
-    glColor3f(1.0, 0.0, 1.0)
-    glVertex3f(1.0, 1.0, -1.0)
-    glVertex3f(1.0, 1.0, 1.0)
-    glVertex3f(1.0, -1.0, 1.0)
-    glVertex3f(1.0, -1.0, -1.0)
+    # Draw the loaded object with computed normals
+    glBegin(GL_TRIANGLES)
+    normals = compute_normals()
+    for i, face in enumerate(obj_faces):
+        for j, vertex in enumerate(face):
+            glNormal3fv(normals[i])
+            glTexCoord2fv(obj_texture_coords[vertex[1] - 1])
+            glVertex3fv(obj_vertices[vertex[0] - 1])
     glEnd()
 
     # Restore the model-view matrix
@@ -100,8 +90,11 @@ def render():
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
 
     # Update the rotation angle
     angle += 1.0
--- testtt.py
+++ testtt.py
@@ -23,7 +23,6 @@ pygame.display.set_caption("Photogrammetry")
 screen = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF | pygame.FULLSCREEN)
 screen.fill((200, 0, 100))
 pygame.display.flip()
-time.sleep(5)
 
 
 glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
@@ -45,22 +44,17 @@ clock = pygame.time.Clock()
 glMatrixMode(GL_PROJECTION)
 glLoadIdentity()
 width, height = viewport
-gluPerspective(90, width/float(height), 1, 100.0)
+gluPerspective(90.0, width/float(height), 1, 100.0)
 glEnable(GL_DEPTH_TEST)
 glMatrixMode(GL_MODELVIEW)
 
-rx, ry, rz = (-90,90,0)
-tx, ty, tz = (0,-5,-5)
-
+rx, ry, rz = (-90,0,0)
+tx, ty, tz = (0,0,-5)
 rotate = move = False
-
-# glTranslatef(tx, ty, tz)
-
 stopwatch = time.time()
 MAX_TIME = 10
+glTranslate(tx, ty, 0)
 while 1:
-    if time.time() - stopwatch > MAX_TIME:
-        break
     clock.tick(30)
     for e in pygame.event.get():
         if e.type == QUIT:
@@ -68,7 +62,7 @@ while 1:
         elif e.type == KEYDOWN and e.key == K_ESCAPE:
             sys.exit()
         # elif e.type == MOUSEBUTTONDOWN:
-        #     if e.button == 4: zpos = max(1, zpos-1)
+        #     if e.button == 4: zpos -= 1
         #     elif e.button == 5: zpos += 1
         #     elif e.button == 1: rotate = True
         #     elif e.button == 3: move = True
@@ -83,50 +77,22 @@ while 1:
         #     if move:
         #         tx += i
         #         ty -= j
-    
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glClearColor(1,1,1,1)
-    # glLoadIdentity()
+    glLoadIdentity()
+
+    # ry += 1
+    # glPushMatrix()
+    glTranslate(tx, ty, tz)
+    # glTranslatef(0.0, 0.0, -5.0)
+    # Rotate around the local Y-axis
+    glRotatef(ry, 0.0, 1.0, 0.0)
+    glRotatef(rx, 1.0, 0.0, 0.0)
+    glRotatef(rz, 0.0, 0.0, 1.0)
 
     ry += 1
-    # rz += 1
-    # rx += 1
-
-
-    glRotatef(ry, 0, 1.0, 0)
-    glRotatef(rz, 0, 0, 1.0)
-    glRotatef(rx, 1.0, 0, 0)
-    glPushMatrix()
-    glTranslatef(tx, ty, tz)
+    
     obj.render()
-    glPopMatrix()
-
-
-    # glPushMatrix()
-    # glTranslatef(tx, ty, tz)
-    # glRotatef(ry, 0, 1.0, 0)
-    # glRotatef(rz, 0, 0, 1.0)
-    # glRotatef(rx, 1.0, 0, 0)
-    # glTranslatef(-tx, -ty, -tz)
-    # obj.render()
     # glPopMatrix()
-
-
-    # glPushMatrix()
-    # glTranslate(-tx, -ty, -tz)
-    # glRotate(ry, 0, 1, 0)
-    # glTranslate(tx, ty, tz)
-    # obj.render()
-    # glPopMatrix()
-
-
-    # RENDER OBJECT
-    # glTranslate(0, 0, 0)
-    # glTranslate(tx/20.0, ty/20.0, - zpos)
-    # glRotate(ry, 1, 0, 0)
-    # glRotate(rx, 0, 1, 0)
-    # glRotate(rz, 0, 0, 1)
-    # glTranslate(tx/20., ty/20., - zpos)
-
-
     pygame.display.flip()
