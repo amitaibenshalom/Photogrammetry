def278af46c4e2c1777e93f71bd15aa752916bf7
Amitai Ben Shalom <amitaiami22@gmail.com>
2024-03-19 14:50:48 +0200

    test

--- outside_screen/consts.py
+++ outside_screen/consts.py
@@ -1,6 +1,6 @@
 
 # photogrammetry_data_path = r'C:\Users\amita\OneDrive - The Bloomfield Science Museum in Jerusalem\photogrammetry_data'
-photogrammetry_data_path = r'C:\Users\Dell\OneDrive - The Bloomfield Science Museum in Jerusalem\photogrammetry_data'
+photogrammetry_data_path = r'G:\My Drive\photogrammetry\photogrammetry_data'
 obj_path = r'output/texturedMesh.obj'
 texture_path = r'output/texture_1001.png'
 model_number = 0
