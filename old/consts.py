ea8f384bbca0fd22c30b7bb51805d35121bcc87f
Amitai Ben Shalom <amitaiami22@gmail.com>
2024-04-29 12:59:14 +0300

    working no buttons

--- old/consts.py
+++ old/consts.py
@@ -1,7 +1,10 @@
 
 photogrammetry_data_path = r'C:\Users\amita\OneDrive - The Bloomfield Science Museum in Jerusalem\photogrammetry_data'
-# photogrammetry_data_path = r'C:\Users\Dell\OneDrive - The Bloomfield Science Museum in Jerusalem\photogrammetry_data'
-obj_path = r'output/texturedMesh.obj'
-texture_path = r'output/texture_1001.png'
+photogrammetry_data_path = r'D:\Amitai_D\museum\photogrammetria\tests\photogrammetry_data_demo'
+obj_path = r'output/texturedMesh.obj'  # DO NOT CHANGE
+texture_path = r'output/texture_1001.png'  # DO NOT CHANGE
+
+borders = [0.1,0.05]  # the precentage of screen (0.1 = 10%) from the sides (width and height axis) that will be used only for buttons (will not move the object)
+
 model_number = 0
 MAX_MODEL_NUMBER = 15
--- old/test2.py
+++ old/test2.py
@@ -53,19 +53,26 @@ def render_model():
     glRotatef(rz, 0.0, 0.0, 1.0)
     obj.render()
 
-def end_model_view():
-    global screen
-    # pygame.display.quit()
-    # pygame.display.init()
-    print("end_model_view called - doing nothing")
+def draw_line(vertices):
+    glColor3f(1.0, 0.0, 0.0)
+    glBegin(GL_LINE_STRIP)
+    for vertex in vertices:
+        glVertex2f(*vertex)
+    glColor3f(1.0, 1.0, 1.0)
+    glEnd()
+
+def render_border():
+    global line_vertices
+    draw_line(line_vertices)
 
 def get_nth_obj_in_folder(folder_path, n):
     items = os.listdir(folder_path)
     if len(items) == 0 or n >= len(items):
-        return None
-    n += 1
+        return None, None
     obj_file_path = os.path.join(folder_path, items[-n], obj_path)
     texture_file_path = os.path.join(folder_path, items[-n], texture_path)
+    if not os.path.exists(obj_file_path) or not os.path.exists(texture_file_path):
+        return None, None
     return obj_file_path, texture_file_path
 
 
@@ -74,6 +81,9 @@ screen_info = pygame.display.Info()
 viewport = (screen_info.current_w,screen_info.current_h)
 width = screen_info.current_w
 height = screen_info.current_h
+# a list of the vertices of the border of the screen from -1 to 1 each axis (x,y)
+line_vertices = [(2*borders[0]-1, 2*borders[1]-1), (2*borders[0]-1, 1-2*borders[1]), (1-2*borders[0], 1-2*borders[1]), (1-2*borders[0], 2*borders[1]-1), (2*borders[0]-1, 2*borders[1]-1)]
+
 
 screen = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF | pygame.FULLSCREEN)
 glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
@@ -105,15 +115,25 @@ glLoadIdentity()
 
 clock = pygame.time.Clock()
 rx, ry, rz = (-90,180,0)
-zpos = -2
+zpos = -4
 obj = None
 
 last_touch = time.time()
 time_to_idle = 10
 idle = False
-model_number = 0
+
 obj_file_path, texture_file_path = get_nth_obj_in_folder(photogrammetry_data_path, model_number)
+print(f"model path: {obj_file_path}")
 init_model(obj_file_path)
+# Render the fixed line
+glPushMatrix()  # Save the current matrix
+glLoadIdentity()  # Reset the matrix to identity
+render_border()
+glPopMatrix()  # Restore the previous matrix
+# Apply transformations to the 3D model
+glRotatef(1, 3, 1, 1)  # Example rotation (replace with your model rotation)
+render_model()  # Render the 3D model
+
 
 running = True
 while running:
@@ -130,7 +150,14 @@ while running:
                 if model_number >= MAX_MODEL_NUMBER or model_number >= len(os.listdir(photogrammetry_data_path)):
                     model_number = 0
                 obj_file_path, texture_file_path = get_nth_obj_in_folder(photogrammetry_data_path, model_number)
-                if (not os.path.exists(obj_file_path)) or (not os.path.exists(texture_file_path)):
+                # print(f"model path: {obj_file_path}")
+                max_iterations = MAX_MODEL_NUMBER
+                while obj_file_path is None:
+                    print("Path not found - skipping one model")
+                    max_iterations -= 1
+                    if max_iterations <= 0:
+                        print("NO MODELS IN FOLDER - must be at least one")
+                        exit()
                     model_number += 1
                     if model_number >= MAX_MODEL_NUMBER or model_number >= len(os.listdir(photogrammetry_data_path)):
                         model_number = 0
@@ -141,14 +168,21 @@ while running:
                 if model_number < 0:
                     model_number = min(MAX_MODEL_NUMBER-1, len(os.listdir(photogrammetry_data_path)) - 1)
                 obj_file_path, texture_file_path = get_nth_obj_in_folder(photogrammetry_data_path, model_number)
-                if (not os.path.exists(obj_file_path)) or (not os.path.exists(texture_file_path)):
+                # print(f"model path: {obj_file_path}")
+                max_iterations = MAX_MODEL_NUMBER
+                while obj_file_path is None:
+                    print("Path not found - skipping one model")
+                    max_iterations -= 1
+                    if max_iterations <= 0:
+                        print("NO MODELS IN FOLDER - must be at least one")
+                        exit()
+                        break
                     model_number -= 1
                     if model_number < 0:
                         model_number = min(MAX_MODEL_NUMBER-1, len(os.listdir(photogrammetry_data_path)) - 1)
                     obj_file_path, texture_file_path = get_nth_obj_in_folder(photogrammetry_data_path, model_number)
-                init_model(obj_file_path)
-        # if mouse is CLICKED and dragged then rotate the model accordingly
-        elif event.type == MOUSEBUTTONDOWN:
+                init_model(obj_file_path)        
+        elif event.type == MOUSEBUTTONDOWN:  # if mouse is CLICKED and dragged then rotate the model accordingly
             last_touch = time.time()
             idle = False
             if event.button == 4:
@@ -158,9 +192,9 @@ while running:
                 # zpos = min(zpos + 1, -1)
                 pass
             elif event.button == 1:
-                x, y = event.pos
-                rx += (y - ry)
-                ry += (x - rx)
+                # x, y = event.pos
+                # rx += (y - ry)
+                # ry += (x - rx)
                 pass
         elif event.type == MOUSEMOTION:
             last_touch = time.time()
@@ -183,22 +217,21 @@ while running:
         pass
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glClearColor(226/255,233/255,241/255,1.0)
 
-    # Draw a textured quad
-    # glBindTexture(GL_TEXTURE_2D, texture)
-    # glBegin(GL_QUADS)
-    # glTexCoord2f(0, 0)
-    # glVertex2f(0, 0)
-    # glTexCoord2f(1, 0)
-    # glVertex2f(width, 0)
-    # glTexCoord2f(1, 1)
-    # glVertex2f(width, height)
-    # glTexCoord2f(0, 1)
-    # glVertex2f(0, height)
-    # glEnd()
+    # Render the fixed line
+    glPushMatrix()  # Save the current matrix
+    glLoadIdentity()  # Reset the matrix to identity
+    render_border()
+    glPopMatrix()  # Restore the previous matrix
+    # Apply transformations to the 3D model
+    glRotatef(1, 3, 1, 1)  # Example rotation (replace with your model rotation)
+    render_model()  # Render the 3D model
 
+
+    # render_model()
+    # render_border()
     pygame.display.flip()
-    # pygame.time.wait(10)    
     clock.tick(30)
 
 
