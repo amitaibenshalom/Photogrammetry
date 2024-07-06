f9cf414e641265d37d30dcb0d2f9a85e90045706
Amitai Ben Shalom <amitaiami22@gmail.com>
2024-07-06 12:13:47 +0300

    Update run_meshroom.py

--- main/run_meshroom.py
+++ main/run_meshroom.py
@@ -8,21 +8,22 @@ output_path_check = None
 def run_meshroom_basic(meshroom_path, input_path, output_path, cache_directory, config_path=None, forceCompute=True):
     global process
     global output_path_check
+    print(input_path)
     os.makedirs(output_path, exist_ok=True)
     output_path_check = output_path
-    meshroom_cmd = [
-        meshroom_path, 
-        '-i', input_path,
-        '-o', output_path,
-        '--cache', cache_directory
-    ]
-
-    # Add params option if a config file is specified
-    if config_path:
-        meshroom_cmd.extend(['--overrides', config_path])
-    if forceCompute:
-        meshroom_cmd.append('--forceCompute')
-
+    # meshroom_cmd = [
+    #     meshroom_path, 
+    #     '--input', input_path,
+    #     '--output', output_path,
+    #     '--cache', cache_directory
+    # ]
+    
+    # # Add params option if a config file is specified
+    # if config_path:
+    #     meshroom_cmd.extend(['--overrides', config_path])
+    # if forceCompute:
+    #     meshroom_cmd.append('--forceCompute')
+    meshroom_cmd = [f'{meshroom_path} --input {input_path} --output {output_path} --cache {cache_directory} --overrides {config_path} --forceCompute']
     try:
         # Run Meshroom command
         process = subprocess.Popen(meshroom_cmd, shell=True)
